0c54879b68444cb137e5389bb694b3c53de232e4
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-01-24 20:21:03 +0530

    [Guru] Welcome to Snake and Ladder Simulator Program

---
+++ Snake_And_Ladder_Simulator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_And_Ladder_Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Simulator program \n");
+            Console.ReadLine();
+        }
+    }
+}
