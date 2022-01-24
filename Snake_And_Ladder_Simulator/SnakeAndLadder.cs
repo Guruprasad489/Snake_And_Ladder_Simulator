42f14da28c2fceeee5ce7688d4a66cb4618524e0
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-01-25 00:23:40 +0530

    [Guru] modified to reach winning position UC4

--- Snake_And_Ladder_Simulator/SnakeAndLadder.cs
+++ Snake_And_Ladder_Simulator/SnakeAndLadder.cs
@@ -8,30 +8,38 @@ namespace Snake_And_Ladder_Simulator
 {
     internal class SnakeAndLadder
     {
-        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100;
         public int player_Position = 0, DiceRoll;
         public void PlaySnakeAndLadder()
         {
             Console.WriteLine("Game starts now...");
             Console.WriteLine("Starting position of player is: " + player_Position);
             Random random = new Random();
-            DiceRoll= random.Next(1,7);
-            Console.WriteLine("Player gets Dice Number: " + DiceRoll);
-            
-            switch(random.Next(0, 3))
+
+            while(player_Position < WINNING_POSITION)
             {
-                case NO_PLAY:
-                    Console.WriteLine("No Play");
-                    break;
-                    
-                case LADDER:
-                    player_Position += DiceRoll;
-                    Console.WriteLine("Ladder: " + player_Position);
-                    break;
-                case SNAKE:
-                    player_Position -= DiceRoll;
-                    Console.WriteLine("Snake: " + player_Position);
-                    break;
+                DiceRoll = random.Next(1, 7);
+                Console.WriteLine("Player gets Dice Number: " + DiceRoll);
+
+                switch (random.Next(0, 3))
+                {
+                    case NO_PLAY:
+                        Console.WriteLine("No Play");
+                        break;
+
+                    case LADDER:
+                        player_Position += DiceRoll;
+                        Console.WriteLine("Ladder: " + player_Position);
+                        break;
+                    case SNAKE:
+                        player_Position -= DiceRoll;
+                        Console.WriteLine("Snake: " + player_Position);
+                        if(player_Position<0)
+                        {
+                            player_Position = 0;
+                        }
+                        break;
+                }
             }
 
         }
